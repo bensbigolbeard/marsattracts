b1075f642233d0321172ece688834432a7348522
bensbigolbeard <ben.white@utexas.edu>
2014-01-10 17:25:40 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MarsAttracts::Application.config.secret_key_base = '0f1d40c8bbd64a545c368ba713fb94f211e0d745e2950d19f6f3c161019b2a93c1dce5c06fbd955fb8509063f6721e9db4020ac01eb2f868fcd2af51196a3c66'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MarsAttracts::Application.config.session_store :cookie_store, key: '_mars-attracts_session'
