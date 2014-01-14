64ad4e28571daad2e5b780e5e49a7cba98385f29
Ben White <bensbigolbeard@gmail.com>
2014-01-14 16:33:49 -0600

    Add ship name to ship table, add basic styling to edit trip info page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140111234650) do
+ActiveRecord::Schema.define(version: 20140114211806) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20140111234650) do
     t.integer  "capacity"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
   create_table "trips", force: true do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,24 +7,24 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 Ship.create!([
-  {speed:1200, capacity:300},
-  {speed:900, capacity:300},
-  {speed:1700, capacity:300}
+  {speed:1200, capacity:300, name: "Serenity"},
+  {speed:900, capacity:400, name: "Enterprise"},
+  {speed:1700, capacity:250, name: "Millenium Falcon"}
 ]);
 
 Ship.find(1).flights.create!([
     {flight_number:'001', 
     departure_time: Date.today, 
     arrival_time: Date.today + 6.months, 
-    origin: "Mega City One", 
-    destination: "Olympus Mons Base Camp", 
+    origin: "Los Angeles", 
+    destination: "Olympus Mons", 
     distance: "a million units"},
 
     {flight_number:'002', 
     departure_time: Date.today, 
     arrival_time: Date.today + 5.months, 
-    origin: "Mega City One", 
-    destination: "Olympus Mons Base Camp", 
+    origin: "Austin", 
+    destination: "Gusev Crater", 
     distance: "a million units"},
 ]);
 
@@ -32,15 +32,15 @@ Ship.find(2).flights.create!([
     {flight_number:'003', 
     departure_time: Date.today, 
     arrival_time: Date.today + 4.months, 
-    origin: "Mega City One", 
-    destination: "Olympus Mons Base Camp", 
+    origin: "Boston", 
+    destination: "Mawrth Vallis", 
     distance: "a million units"},
 
     {flight_number:'004', 
     departure_time: Date.today, 
     arrival_time: Date.today + 6.months, 
-    origin: "Mega City One", 
-    destination: "Olympus Mons Base Camp", 
+    origin: "Chicago", 
+    destination: "Valles Marineris", 
     distance: "a million units"},
 ]);    
 
@@ -48,8 +48,8 @@ Ship.find(3).flights.create!([
     {flight_number:'005', 
     departure_time: Date.today, 
     arrival_time: Date.today + 3.months, 
-    origin: "Mega City One", 
-    destination: "Olympus Mons Base Camp", 
+    origin: "Atlanta", 
+    destination: "Utopia Planitia", 
     distance: "a million units"},
 
 ]);
